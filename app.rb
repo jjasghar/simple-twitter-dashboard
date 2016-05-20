9fc6f94f8aba5b46702ba5d7c6f5ceab0b1ba35f
JJ Asghar <jj@chef.io>
2016-05-19 19:46:37 -0500

    WIP: got three tweets to show up, need to default to true if nothing is there

--- app.rb
+++ app.rb
@@ -4,7 +4,12 @@ require 'yaml'
 
 config = YAML.load_file('config.yml')
 
-follower = [config['follower']]
+# if config['follower'] = nil
+#   follower = [","]
+# else
+  follower = config['follower'].split(",")
+#end
+
 hashtag = config['hashtag']
 
 set :bind, config['bind_address']
@@ -35,8 +40,29 @@ get '/' do
           :refresh => config['refresh']
         }
 
+  elsif follower
+    follower.each do |f|
+      client.user_timeline(f)[0..0].each do |tweet|
+        tweets[tweet.id] = {
+          name: tweet.user.name,
+          author: tweet.user.screen_name,
+          tweet: tweet.full_text,
+          gravitar: tweet.user.profile_image_url,
+          retweeted: tweet.retweet_count
+        }
+
+        tweets[tweet.id][:image] = tweet.media[0].media_uri.to_s if tweet.media[0]
+      end
+    end
+
+    erb :index, :locals => {
+          :output_tweets => tweets,
+          :topic => follower[0],
+          :refresh => config['refresh']
+        }
+
   else
-    client.user_timeline("@cnnbrk")[0..0].each do |tweet|
+    client.search(true, result_type: "recent").take(1).each do |tweet|
       tweets[tweet.id] = {
         name: tweet.user.name,
         author: tweet.user.screen_name,
@@ -50,8 +76,9 @@ get '/' do
 
     erb :index, :locals => {
           :output_tweets => tweets,
-          :topic => follower[0],
+          :topic => hashtag,
           :refresh => config['refresh']
         }
+
   end
 end
